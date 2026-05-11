dbd040e9e64c841636e582dae8bdf73ebcdfc117
kao <music0227@gmail.com>
2026-05-11 10:52:18 +0800

    update: configure markview toggle

--- lua/kickstart/plugins/markview.lua
+++ lua/kickstart/plugins/markview.lua
@@ -2,6 +2,14 @@
 return {
   'OXY2DEV/markview.nvim',
   lazy = false,
+  opts = {
+    preview = {
+      enable = false,
+    },
+  },
+  keys = {
+    { '<leader>mt', '<cmd>Markview toggle<cr>', desc = '[M]arkview [T]oggle preview' },
+  },
 
   -- Completion for `blink.cmp`
   -- dependencies = { "saghen/blink.cmp" },
